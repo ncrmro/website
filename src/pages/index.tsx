74c8d7366d8bd6470f21c6c56bb54ae9928782b4
Nicholas Romero <ncrmro@gmail.com>
2019-03-07 18:49:22 -0600

    chore: comment out old text

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,14 +6,14 @@ export default withSiteData(() => (
     <h1>
       Welcome to React-Static <br /> + TypeScript
     </h1>
-    <p>
-      Learn{' '}
-      <a href="https://github.com/sw-yx/react-typescript-cheatsheet">
-        React + TypeScript
-      </a>
-    </p>
-    <p>
-      <a href="https://twitter.com/swyx">Report issues with this template</a>
-    </p>
+    {/*<p>*/}
+      {/*Learn{' '}*/}
+      {/*<a href="https://github.com/sw-yx/react-typescript-cheatsheet">*/}
+        {/*React + TypeScript*/}
+      {/*</a>*/}
+    {/*</p>*/}
+    {/*<p>*/}
+      {/*<a href="https://twitter.com/swyx">Report issues with this template</a>*/}
+    {/*</p>*/}
   </div>
 ))
