166d0599a3c7ef2103e0908aebb3b6c09bfff41e
Nicholas Romero <ncrmro@gmail.com>
2019-03-09 23:33:09 -0600

    chore: prettier

--- src/onStart.ts
+++ src/onStart.ts
@@ -1,5 +1,5 @@
-import * as Sentry from "@sentry/browser";
-import { ENV } from "./types";
+import * as Sentry from '@sentry/browser'
+import { ENV } from './types'
 
 declare const process: {
   ['env']: ENV
