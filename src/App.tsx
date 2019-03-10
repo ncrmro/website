eae65d437e48ab962cca9dc160092d3b9440d659
Nicholas Romero <ncrmro@gmail.com>
2019-03-10 09:28:19 -0500

    chore: remove nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
 import React from 'react'
 import { Root, Routes } from 'react-static'
-import { Link } from '@reach/router'
 import './app.css'
 // import FancyDiv from '@components/FancyDiv'
 
 function App() {
   return (
     <Root>
-      <nav>
-        <Link to="/">Home</Link>
-      </nav>
       <div className="content">
         <Routes />
       </div>
