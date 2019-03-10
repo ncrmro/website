4fa6a30e0f760fd6d63837ddd477e2331c2c182a
Nicholas Romero <ncrmro@gmail.com>
2019-03-10 00:06:10 -0600

    fix: key

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
-import { Link } from "@reach/router";
-import React from "react";
-import { withSiteData } from "react-static";
+import { Link } from '@reach/router'
+import React from 'react'
+import { withSiteData } from 'react-static'
 
 export default withSiteData(
   ({ title, posts }: { title: string; posts: any }) => (
@@ -8,11 +8,9 @@ export default withSiteData(
       <h1>{title}</h1>
       <ul>
         {posts.map((post: any) => (
-            <li>
-              <Link key={post.id} to={`/posts/${post.id}`}>
-                {post.title}
-              </Link>
-            </li>
+          <li key={post.id}>
+            <Link to={`/posts/${post.id}`}>{post.title}</Link>
+          </li>
         ))}
       </ul>
     </div>
