12654eb2397fa339a5956e2e3560c45654d0ed67
Nicholas Romero <ncrmro@gmail.com>
2019-03-09 23:52:13 -0600

    feat: white background, no list types

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,20 @@
-import React from 'react'
-import { withSiteData } from 'react-static'
-import { Link } from '@reach/router'
+import { Link } from "@reach/router";
+import React from "react";
+import { withSiteData } from "react-static";
 
 export default withSiteData(
   ({ title, posts }: { title: string; posts: any }) => (
     <div style={{ textAlign: 'center' }}>
       <h1>{title}</h1>
-      {posts.map((post: any) => (
-        <Link key={post.id} to={`/posts/${post.id}`}>
-          {post.title}
-        </Link>
-      ))}
+      <ul>
+        {posts.map((post: any) => (
+            <li>
+              <Link key={post.id} to={`/posts/${post.id}`}>
+                {post.title}
+              </Link>
+            </li>
+        ))}
+      </ul>
     </div>
   ),
 )
