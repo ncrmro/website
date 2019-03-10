8b8d363c56a07c467bf89dbb6f35d70704e8c4e8
Nicholas Romero <ncrmro@gmail.com>
2019-03-10 09:28:02 -0500

    feat: add post link className

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,7 @@ export default withSiteData(
       <h1>{title}</h1>
       <ul>
         {posts.map((post: any) => (
-          <li key={post.id}>
+          <li key={post.id} className="post_link">
             <Link to={`/posts/${post.id}`}>{post.title}</Link> - {post.datePosted}
             <br/>
             {post.description}
