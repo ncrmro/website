3e22b1fbe7d3cbd9388897f5b218e73971600b7d
Nicholas Romero <ncrmro@gmail.com>
2020-06-15 12:14:51 -0500

    feat: add linkdin

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,23 +1,28 @@
 import React from 'react'
-import { OutboundLink } from "gatsby-plugin-google-analytics"
+import { OutboundLink } from 'gatsby-plugin-google-analytics'
 
 const Footer = () => (
   <div id="footer">
     <div className="inner">
       <ul className="icons">
         <li>
-
-          <OutboundLink href="https://twitter.com/ncrmro" className="icon fa-twitter">
+          <OutboundLink
+            href="https://twitter.com/ncrmro"
+            className="icon fa-twitter"
+          >
             <span className="label">Twitter</span>
           </OutboundLink>
         </li>
         <li>
-          <OutboundLink href="https://github.com/ncrmro" className="icon fa-github">
+          <OutboundLink
+            href="https://github.com/ncrmro"
+            className="icon fa-github"
+          >
             <span className="label">Github</span>
           </OutboundLink>
         </li>
         <li>
-          <OutboundLink href="#" className="icon fa-linkedin">
+          <OutboundLink href="https://www.linkedin.com/in/ncrmro/" className="icon fa-linkedin">
             <span className="label">Linkdin</span>
           </OutboundLink>
         </li>
