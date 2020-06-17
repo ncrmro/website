004c143c847c7eff040992506730a6672cd414cd
Nicholas Romero <ncrmro@gmail.com>
2020-06-17 12:07:59 -0500

    feat: update post file script generator

--- new_post.zsh
+++ new_post.zsh
@@ -7,7 +7,7 @@ post_tile=$1
 post_title_lower=$(echo "$post_tile" | awk '{print tolower($0)}')
 post_title_underscored="${post_title_lower// /_}"
 
-posts_dir=static/posts
+posts_dir=posts
 
 # 2020_06_14_testing_new_post_script.md
 post_file_name=$(date +'%Y_%m_%d')_$post_title_underscored.md
@@ -16,9 +16,11 @@ echo Creating file.. "$post_file_name"
 
 cat <<EOT >> $posts_dir/$post_file_name
 ---
+slug: '/posts/$post_file_name'
 title: $post_tile
-datePosted: ${current_date}
-description: A simple hello world post.
+datePosted: '${current_date}'
+description: CHANGEME
+tags: ['CHANGEME']
 ---
 
 Hello World
