7c091c2dea8079654b0b440c651e3eb1c41cb743
Nicholas Romero <ncrmro@gmail.com>
2020-12-23 19:06:34 -0600

    feat: add debug

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,10 @@
 import "tailwindcss/tailwind.css";
 import SiteLayout from "@components/SiteLayout";
 import { initializeAnalaytics } from "@utils/analytics";
-import config from '@utils/config'
+import config from "@utils/config";
 
 function MyApp({ Component, pageProps }) {
-  console.debug(config)
+  console.debug(config);
   initializeAnalaytics();
   return (
     <SiteLayout>
