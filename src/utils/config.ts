460d78a7bb336d728553bca2360d535f031c0aca
Nicholas Romero <ncrmro@gmail.com>
2021-01-19 17:01:44 -0600

    chore: disable debug

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -11,8 +11,8 @@ interface Config {
   };
 }
 
-// const debug = process.env.WEB_DEBUG === "true";
-const debug = true;
+const debug = process.env.WEB_DEBUG === "true";
+// const debug = true;
 
 const config: Config = {
   production: process.env.NODE_ENV === "production",
