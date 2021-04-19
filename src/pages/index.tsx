e4d2d0cbebaad85c337cd88e64f0bad5093309a7
Nicholas Romero <ncrmro@gmail.com>
2021-04-19 14:03:45 -0500

    chore: clean up imports

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,36 +1,10 @@
-import GithubIcon from "@components/Icons/Github";
-import InstagramIcon from "@components/Icons/Instagram";
-import LinkedinIcon from "@components/Icons/Linkedin";
-import TwitterIcon from "@components/Icons/Twitter";
-import PageLayout from "@components/PageLayout";
-import { Post } from "@utils/markdown";
+import React from "react";
 import { GetStaticProps } from "next";
 import Head from "next/head";
-import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import PageLayout from "@components/PageLayout";
+import { Post } from "@utils/markdown";
 
-const PostCard: React.FC<Post> = (props) => (
-  <div className="flex-1 bg-white flex flex-col justify-between">
-    <div className="flex-1">
-      <Link href={`/posts/${props.slug}`}>
-        <a href="#" className="block mt-2">
-          <p className="text-xl font-semibold text-gray-900">{props.title}</p>
-          <p className="mt-3 text-base text-gray-500">{props.description}</p>
-        </a>
-      </Link>
-    </div>
-    {/*<div className="mt-6 flex items-center">*/}
-    {/*  <div className="ml-3">*/}
-    {/*    <div className="flex space-x-1 text-sm text-gray-500">*/}
-    {/*      <time dateTime="2020-03-16">Mar 16, 2020</time>*/}
-    {/*      <span aria-hidden="true">&middot;</span>*/}
-    {/*      <span>6 min read</span>*/}
-    {/*    </div>*/}
-    {/*  </div>*/}
-    {/*</div>*/}
-  </div>
-);
 
 function Home(props) {
   return (
