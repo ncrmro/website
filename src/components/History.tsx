7d01eff8b773c271474a074c72c2ba506df7bb13
Nicholas Romero <ncrmro@gmail.com>
2021-06-04 07:09:58 -0500

    chore: upgrade packages, try removing netlify and add missing key

--- src/components/History.tsx
+++ src/components/History.tsx
@@ -6,6 +6,7 @@ const History: React.FC<{ jobs: JobDocument[] }> = (props) => (
   <ul>
     {props.jobs.map((item, idx) => (
       <HistoryItem
+        key={item.title}
         job={item}
         // Show vertical line on all but last item
         verticalLine={props.jobs.length !== idx + 1}
