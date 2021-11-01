7de59db19d010ff60debbdfa91db11b37d79b15d
Nicholas Romero <ncrmro@gmail.com>
2021-10-31 22:09:47 -0500

    refactor: history page

--- src/routes/Resume/History.tsx
+++ src/routes/Resume/History.tsx
@@ -1,19 +1,34 @@
-import HistoryItem from "./HistoryItem";
+import MarkdownRenderer from "@components/MarkdownRenderer";
+import SmallBadge from "@components/SmallBadge";
+import TechUrls from "@utils/techUrls";
 import { JobDocument } from "@utils/documents";
 import React from "react";
-import styles from "./Resume.module.css";
+import styles from "./History.module.css";
 
 const History: React.FC<{ jobs: JobDocument[]; className?: string }> = (
   props
 ) => (
-  <ul className={styles.history}>
-    {props.jobs.map((item, idx) => (
-      <li key={item.title} className={styles.historyItem}>
-        <HistoryItem
-          key={item.title}
-          job={item}
-          pageBreak={[3].includes(idx)}
-        />
+  <ul className={styles.historyList}>
+    {props.jobs.map((job, idx) => (
+      <li className={styles.historyItem}>
+        <img className={styles.img} src={job.favicon} />
+        <div className={styles.historyItemTitle}>
+          <div>
+            <h2>{job.role}</h2>
+            <h3>{job.url ? <a href={job.url}>{job.title}</a> : job.title}</h3>
+          </div>
+          <div>
+            {job.start} {job.end}
+          </div>
+        </div>
+        <div>
+          <MarkdownRenderer content={job.body} />
+        </div>
+        <div className={styles.badges}>
+          {job.tech?.map((tech) => (
+            <SmallBadge key={tech} children={tech} link={TechUrls[tech]} />
+          ))}
+        </div>
       </li>
     ))}
   </ul>
