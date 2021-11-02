ab20423444ce07786a9298a6c3b8741b98d1c5a3
Nicholas Romero <ncrmro@gmail.com>
2021-11-01 19:02:58 -0500

    refactor: markdown renderer and code block

--- src/components/MarkdownRenderer.tsx
+++ src/components/MarkdownRenderer.tsx
@@ -6,66 +6,62 @@ import { Remark } from "react-remark";
 import styles from "./MarkdownRenderer.module.css";
 
 const MarkdownRenderer: React.FC<{ content: string }> = (props) => (
-  <div className={styles.root}>
-    <Remark
-      rehypeReactOptions={{
-        components: {
-          pre: (props) => {
-            const className: string = props.children[0].props?.className;
-            if (className && className.includes("language-")) {
-              let language = className.replace("language-", "");
-              const code = props.children[0].props.children[0];
-              if (language === "typescript") {
-                language = "jsx";
-              }
-
-              return <CodeBlock code={code} language={language as Language} />;
+  <Remark
+    rehypeReactOptions={{
+      components: {
+        pre: (props) => {
+          const className: string = props.children[0].props?.className;
+          if (className && className.includes("language-")) {
+            let language = className.replace("language-", "");
+            const code = props.children[0].props.children[0];
+            if (language === "typescript") {
+              language = "jsx";
             }
 
-            return <pre>{props.children}</pre>;
-          },
-          h2: (props) => (
-            <h2
-              id={slugify(
-                typeof props.children[0] === "string"
-                  ? props.children[0]
-                  : props.children[0].props.children[0]
-              )}
-              className="font-semibold text-2xl pt-4"
-            >
-              {props.children}
-            </h2>
-          ),
-          h3: (props) => (
-            <h3
-              id={slugify(
-                typeof props.children[0] === "string"
-                  ? props.children[0]
-                  : props.children[0].props.children[0]
-              )}
-              className="font-semibold text-xl  pt-4"
-            >
-              {props.children}
-            </h3>
-          ),
-          img: (props) => (
-            <img src={props.src as string} alt={props.alt as string} />
-          ),
-          a: (props: { href; children }) => (
-            <a href={props.href}>{props.children}</a>
-          ),
-          p: (props) => <p>{props.children}</p>,
-          ul: (props) => (
-            <ul className="list-disc list-inside flex flex-col gap-1">
-              {props.children}
-            </ul>
-          ),
+            return <CodeBlock code={code} language={language as Language} />;
+          }
+
+          return <pre>{props.children}</pre>;
         },
-      }}
-    >
-      {props.content}
-    </Remark>
-  </div>
+        h2: (props) => (
+          <h2
+            id={slugify(
+              typeof props.children[0] === "string"
+                ? props.children[0]
+                : props.children[0].props.children[0]
+            )}
+          >
+            {props.children}
+          </h2>
+        ),
+        h3: (props) => (
+          <h3
+            id={slugify(
+              typeof props.children[0] === "string"
+                ? props.children[0]
+                : props.children[0].props.children[0]
+            )}
+          >
+            {props.children}
+          </h3>
+        ),
+        img: (props) => (
+          <img
+            src={props.src as string}
+            alt={props.alt as string}
+            className={styles.link}
+          />
+        ),
+        a: (props: { href; children }) => (
+          <a href={props.href}>{props.children}</a>
+        ),
+        p: (props) => <p>{props.children}</p>,
+        ul: (props) => <ul className={styles.list}>{props.children}</ul>,
+      },
+    }}
+  >
+    {props.content}
+  </Remark>
 );
 
 export default MarkdownRenderer;
