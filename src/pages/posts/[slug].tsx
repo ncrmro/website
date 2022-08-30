bb83af38dff473bd4771cafd5c7992a703fd3534
Nicholas Romero <ncrmro@gmail.com>
2022-08-30 15:02:47 -0500

    fix: make static for netlify..

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -1,24 +1,35 @@
-import { getDocumentBySlug } from "@quiescent/server";
-import { PostRoute } from "@routes/Posts/PostRoute";
-import React from "react";
-import { GetStaticProps } from "next";
-import { serialize } from "next-mdx-remote/serialize";
-import { MDXPost, Post } from "../../types";
+import { getDocumentBySlug, getDocumentSlugs } from '@quiescent/server'
+import { PostRoute } from '@routes/Posts/PostRoute'
+import React from 'react'
+import { GetStaticProps, GetStaticPaths } from 'next'
+import { serialize } from 'next-mdx-remote/serialize'
+import { MDXPost, Post } from '../../types'
 
 const PostPage: React.FC<{ post: MDXPost }> = (props) => {
-  return <PostRoute post={props.post} />;
-};
+    return <PostRoute post={props.post} />
+}
 
-export const getServerSideProps: GetStaticProps<{ post: MDXPost }> = async (
-  context
+export const getStaticProps: GetStaticProps<{ post: MDXPost }> = async (
+    context,
 ) => {
-  if (typeof context.params?.slug !== "string") throw "Slug was not defined";
-  const post = await getDocumentBySlug<Post>("posts", context.params.slug);
-  if (!post) throw "Error loading post";
+    if (typeof context.params?.slug !== 'string') throw 'Slug was not defined'
+    const post = await getDocumentBySlug<Post>('posts', context.params.slug)
+    if (!post) throw 'Error loading post'
 
-  return {
-    props: { post: { ...post, content: await serialize(post.content) } },
-  };
-};
+    return {
+        props: { post: { ...post, content: await serialize(post.content) } },
+    }
+}
 
-export default PostPage;
+export const getStaticPaths: GetStaticPaths = async () => {
+    const slugs = await getDocumentSlugs('posts', 'dynamic')
+    return {
+        paths: slugs.map((slug) => ({
+            params: {
+                slug,
+            },
+        })), fallback: false,
+    }
+}
+
+export default PostPage
