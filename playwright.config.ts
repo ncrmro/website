b08860b4cec847ea01b8397c932e60de73676226
Nicholas Romero <ncrmro@gmail.com>
2023-07-30 08:19:22 -0600

    feat(ci):

--- playwright.config.ts
+++ playwright.config.ts
@@ -78,9 +78,11 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  webServer: {
-    command: "pnpm run start",
-    url: "http://127.0.0.1:3000",
-    reuseExistingServer: !process.env.CI,
-  },
+  webServer: process.env.CI
+    ? {
+        command: "pnpm run start",
+        url: "http://127.0.0.1:3000",
+        reuseExistingServer: !process.env.CI,
+      }
+    : undefined,
 });
---
+++ tests/example.spec.ts
@@ -0,0 +1,49 @@
+import { test } from "@playwright/test";
+
+test("login page", async ({ page }) => {
+  page.on("console", console.log);
+  const email = `jdoe@example.com`;
+  await page.goto("/login");
+  await page.getByPlaceholder("email").fill(email);
+  await page.getByPlaceholder("password").fill("password");
+  await page.locator("button", { hasText: "Login" }).click();
+  await page.waitForURL("/dashboard");
+});
+
+test("sign up page", async ({ page }) => {
+  page.on("console", console.log);
+  const time = Date.now();
+  const username = `user${time}`;
+  const email = `${username}@test.com`;
+  await page.goto("/signup");
+  await page.getByPlaceholder("email").fill(email);
+  await page.getByPlaceholder("password").fill("testpassword");
+  await page.locator("button", { hasText: "Submit" }).click();
+  await page.waitForURL("/dashboard");
+});
+
+test("create posts", async ({ page }) => {
+  page.on("console", console.log);
+  await page.goto("/posts/new");
+  await page.waitForURL((url) =>
+    url.toString().includes("/login?redirect=%2Fposts%2Fnew")
+  );
+  const email = `jdoe@example.com`;
+  await page.getByPlaceholder("email").fill(email);
+  await page.getByPlaceholder("password").fill("password");
+  await page.locator("button", { hasText: "Login" }).click();
+  // Create a new post
+  await page.waitForURL("/posts/new");
+  const postTitle = `Hello World ${Date.now()}`;
+  await page.getByLabel("Title").fill(postTitle);
+  await page.getByLabel("Body").fill("Hello World");
+  await page.locator("button", { hasText: "Submit" }).click();
+  await page.waitForURL(/\/posts\/hello-world-\d*/);
+  // Edit the post
+  await page.locator("a", { hasText: "Edit" }).click();
+  await page.waitForURL(/\/posts\/hello-world-\d*\/edit/);
+  await page.getByLabel("Body").fill("Hello World Edit");
+  await page.locator("button", { hasText: "Submit" }).click();
+  await page.waitForURL(/\/posts\/hello-world-\d*/);
+  await page.locator("#post-body", { hasText: "Hello World Edit" }).waitFor();
+});
