3f88f6756191cc758af0b55f90bf82521365e21f
Nicholas Romero <ncrmro@gmail.com>
2023-07-31 20:01:15 -0600

    feat(database): update schema to account for tags

--- database/migrations/0001-posts.sql
+++ database/migrations/0001-posts.sql
@@ -1,15 +1,27 @@
+DROP TABLE IF EXISTS posts_tags;
+DROP TABLE IF EXISTS tags;
+CREATE TABLE tags
+(
+    id         text                                NOT NULL PRIMARY KEY DEFAULT (uuid()),
+    value       text                                NOT NULL UNIQUE CHECK (value REGEXP '^[a-z0-9\s]+$'),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
+
+
+
 DROP TABLE IF EXISTS posts;
 CREATE TABLE posts
 (
-    id           text                                NOT NULL PRIMARY KEY DEFAULT (uuid()),
-    user_id      int                                 NOT NULL,
-    title        text                                NOT NULL UNIQUE,
-    body         text                                NOT NULL,
-    slug         text UNIQUE                         NOT NULL CHECK (slug REGEXP '^[\w-]*$'),
-    published    integer   NOT NULL DEFAULT FALSE,
+    id           text        NOT NULL PRIMARY KEY DEFAULT (uuid()),
+    user_id      int         NOT NULL,
+    title        text        NOT NULL UNIQUE,
+    body         text        NOT NULL,
+    slug         text UNIQUE NOT NULL CHECK (slug REGEXP '^[\w-]*$'),
+    published    integer     NOT NULL             DEFAULT FALSE,
     publish_date TIMESTAMP,
-    created_at   TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    updated_at   TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    created_at   TIMESTAMP                        DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at   TIMESTAMP                        DEFAULT CURRENT_TIMESTAMP NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users (id)
 );
 
@@ -24,7 +36,9 @@ CREATE TRIGGER posts_update_timestamp_trigger
     AFTER UPDATE
     ON posts
 BEGIN
-    UPDATE posts SET created_at = old.created_at AND updated_at = CURRENT_TIMESTAMP WHERE id = new.id;
+    UPDATE posts
+    SET created_at = old.created_at AND updated_at = CURRENT_TIMESTAMP
+    WHERE id = new.id;
 END;
 
 CREATE TRIGGER posts_insert_slugify_title
@@ -41,3 +55,11 @@ CREATE TRIGGER posts_update_slugify_title
 BEGIN
     UPDATE posts SET slug = slugify(title) WHERE id = new.id;
 END;
+
+CREATE TABLE posts_tags
+(
+    id      text NOT NULL PRIMARY KEY DEFAULT (uuid()),
+    post_id int  NOT NULL,
+    tag_id  int  NOT NULL,
+    unique (post_id, tag_id)
+);
