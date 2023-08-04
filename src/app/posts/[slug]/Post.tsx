b6f0b525415e4ef6a2f8cf3a88a51d0077db5c33
Nicholas Romero <ncrmro@gmail.com>
2023-08-04 14:25:03 -0600

    fix: tests

--- src/app/posts/[slug]/Post.tsx
+++ src/app/posts/[slug]/Post.tsx
@@ -3,5 +3,9 @@ import React from "react";
 import { MDXRemote } from "next-mdx-remote";
 
 export default function Post(props: { source: any }) {
-  return <MDXRemote {...props.source} />;
+  return (
+    <div id="post-body">
+      <MDXRemote {...props.source} />
+    </div>
+  );
 }
