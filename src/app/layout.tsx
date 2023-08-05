95f25d4ca839a66cfcbcead58b2b19bfe4feaf59
Nicholas Romero <ncrmro@gmail.com>
2023-08-04 22:53:03 -0600

    feat: style

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import "./globals.css";
+import { headers } from "next/headers";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import React from "react";
-import "./global.css";
 import { useViewer } from "@/lib/auth";
 
 export const metadata = {
@@ -16,17 +17,18 @@ export default async function RootLayout({
   children: React.ReactNode;
 }) {
   const viewer = await useViewer();
+
+  const referer = headers();
+  console.log(referer);
   return (
     <html lang="en">
       <body>
-        <nav id="navbar" className="flex justify-between w-full">
-          <div>
-            <Link href="/">NSLITE</Link>
-          </div>
-          <div>
-            <Link href="/posts">Posts</Link>
-            {!viewer && <Link href="/login">Sign in</Link>}
-          </div>
+        <nav id="navbar" className="flex justify-center items-center h-16">
+          <Link href="/">NCRMRO</Link>
+          {/*<div>*/}
+          {/*  <Link href="/posts">Posts</Link>*/}
+          {/*  {!viewer && <Link href="/login">Sign in</Link>}*/}
+          {/*</div>*/}
         </nav>
         <main className="w-full flex-col items-center p-4">{children}</main>
       </body>
--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,24 +1,127 @@
 import { db } from "@/lib/database";
-import Link from "next/link";
+import React from "react";
 
-export default async function Posts() {
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
+
+function PostItem(props: {
+  post: {
+    slug: string;
+    title: string;
+    description: string;
+    publish_date?: string;
+  };
+  evenRow: boolean;
+  index: string;
+}) {
+  return (
+    <li key={props.post.slug} className="flex">
+      <div className="relative pb-8">
+        {props.evenRow ? (
+          <span
+            className="absolute left-5 top-5 -ml-px h-full w-0.5 bg-gray-200"
+            aria-hidden="true"
+          />
+        ) : null}
+        <div className="relative flex items-start space-x-3 pl-10">
+          {props.post.description ? (
+            <>
+              <div className="">
+                {/*<img*/}
+                {/*  className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-400 ring-8 ring-white"*/}
+                {/*  src={props.post.imageUrl}*/}
+                {/*  alt=""*/}
+                {/*/>*/}
+
+                <span className="absolute -bottom-0.5 -right-1 rounded-tl bg-white px-0.5 py-px">
+                  {/*<ChatBubbleLeftEllipsisIcon*/}
+                  {/*  className="h-5 w-5 text-gray-400"*/}
+                  {/*  aria-hidden="true"*/}
+                  {/*/>*/}
+                </span>
+              </div>
+              <div className="min-w-0 flex-1">
+                <div>
+                  <div className="text-sm">
+                    <a
+                      href={`/posts/${props.post.slug}`}
+                      className="font-medium text-gray-900"
+                    >
+                      {props.post.title}
+                    </a>
+                  </div>
+                  <p className="mt-0.5 text-sm text-gray-500">
+                    {props.post.publish_date}
+                  </p>
+                </div>
+                <div className="mt-2 text-sm text-gray-700">
+                  <p>{props.post.description}</p>
+                </div>
+              </div>
+            </>
+          ) : (
+            <>
+              <div>
+                <div className="relative px-1">
+                  <div className="flex h-8 w-8 items-center justify-center rounded-full bg-gray-100 ring-8 ring-white">
+                    {/*<UserCircleIcon*/}
+                    {/*  className="h-5 w-5 text-gray-500"*/}
+                    {/*  aria-hidden="true"*/}
+                    {/*/>*/}
+                  </div>
+                </div>
+              </div>
+              <div className="min-w-0 flex-1 py-1.5">
+                {/*<div className="text-sm text-gray-500">*/}
+                {/*  <a*/}
+                {/*    href={props.post.person.href}*/}
+                {/*    className="font-medium text-gray-900"*/}
+                {/*  >*/}
+                {/*    {props.post.person.name}*/}
+                {/*  </a>{" "}*/}
+                {/*  assigned{" "}*/}
+                {/*  <a*/}
+                {/*    href={props.post.assigned.href}*/}
+                {/*    className="font-medium text-gray-900"*/}
+                {/*  >*/}
+                {/*    {props.post.assigned.name}*/}
+                {/*  </a>{" "}*/}
+                {/*  <span className="whitespace-nowrap">{props.post.date}</span>*/}
+                {/*</div>*/}
+              </div>
+            </>
+          )}
+        </div>
+      </div>
+    </li>
+  );
+}
+
+export async function Posts() {
   const posts = await db
     .selectFrom("posts")
-    .select(["slug", "title", "description", "body"])
+    .select(["slug", "title", "description", "body", "publish_date"])
     .orderBy("publish_date", "desc")
     .where("published", "=", 1)
     .execute();
   return (
+    <div className="flow-root">
+      <ul role="list" className="-mb-8">
+        {posts.map((post, index) => (
+          <PostItem post={post} evenRow={index !== posts.length - 1} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default async function PostsPage() {
+  const posts = await Posts();
+  return (
     <div>
       <h1>Posts</h1>
-      <ol>
-        {posts.map((post) => (
-          <li key={post.slug}>
-            <Link href={`/posts/${post.slug}`}>{post.title}</Link>
-            <p>{post.description}</p>
-          </li>
-        ))}
-      </ol>
+      {posts}
     </div>
   );
 }
