985bc0c9b3bbe195ad9e0321b7ea8b2e401d33e2
Nicholas Romero <ncrmro@gmail.com>
2023-08-12 06:51:00 -0600

    fix: slug regex

--- src/app/posts/form.tsx
+++ src/app/posts/form.tsx
@@ -103,7 +103,8 @@ export default function PostForm(props: {
                   type="text"
                   name="slug"
                   id="slug"
-                  pattern={"/^[a-z0-9-]*/"}
+                  title="Slug can only include lower case letters, numbers and dashes."
+                  pattern={"^[a-z0-9\\-]*$"}
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   value={state?.slug}
                   onChange={(e) =>
