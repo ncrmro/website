481e60f4795ca218e08c0243f0a311178f69a498
Nicholas Romero <ncrmro@gmail.com>
2023-08-15 08:39:33 -0600

    chore:

--- src/app/api/posts/uploads/route.ts
+++ src/app/api/posts/uploads/route.ts
@@ -15,38 +15,38 @@ export async function GET(req: NextRequest) {
   return NextResponse.json({ files: fileNames });
 }
 
-// export async function POST(req: NextRequest) {
-//   const viewer = await useViewer();
-//   const formData = await req.formData();
-//   const postId = formData.get("postId");
-//   if (!viewer || !postId) throw new Error("Viewer and postId must be defined");
-//   try {
-//     await db
-//       .selectFrom("posts")
-//       .select("id")
-//       .where("id", "=", postId)
-//       .where("user_id", "=", viewer.id)
-//       .executeTakeFirstOrThrow();
-//   } catch (e) {
-//     throw new Error("Only owners of the post can upload media.");
-//   }
-//
-//   const postUploadsDirectory = `${uploadDirectory}/${postId}`;
-//   try {
-//     await fs.mkdir(postUploadsDirectory, { recursive: true });
-//   } catch (e) {}
-//
-//   async function writeFile(value: FormDataEntryValue) {
-//     const isFile = typeof value == "object";
-//     if (isFile) {
-//       const file = value as File as unknown as File;
-//       const bytes = await file.arrayBuffer();
-//       const buffer = Buffer.from(bytes);
-//       await fs.writeFile(`${postUploadsDirectory}/${file.name}`, buffer);
-//     }
-//   }
-//
-//   await Promise.all(formData.getAll("files").map(writeFile));
-//
-//   return NextResponse.json({ status: "ok" });
-// }
+export async function POST(req: NextRequest) {
+  const viewer = await useViewer();
+  const formData = await req.formData();
+  const postId = formData.get("postId");
+  if (!viewer || !postId) throw new Error("Viewer and postId must be defined");
+  try {
+    await db
+      .selectFrom("posts")
+      .select("id")
+      .where("id", "=", postId)
+      .where("user_id", "=", viewer.id)
+      .executeTakeFirstOrThrow();
+  } catch (e) {
+    throw new Error("Only owners of the post can upload media.");
+  }
+
+  const postUploadsDirectory = `${uploadDirectory}/${postId}`;
+  try {
+    await fs.mkdir(postUploadsDirectory, { recursive: true });
+  } catch (e) {}
+
+  async function writeFile(value: FormDataEntryValue) {
+    const isFile = typeof value == "object";
+    if (isFile) {
+      const file = value as File as unknown as File;
+      const bytes = await file.arrayBuffer();
+      const buffer = Buffer.from(bytes);
+      await fs.writeFile(`${postUploadsDirectory}/${file.name}`, buffer);
+    }
+  }
+
+  await Promise.all(formData.getAll("files").map(writeFile));
+
+  return NextResponse.json({ status: "ok" });
+}
