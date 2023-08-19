91feb065698b30a07adde569bc70137e65f68c7a
Nicholas Romero <ncrmro@gmail.com>
2023-08-18 19:57:50 -0600

    chore:

--- playwright.config.ts
+++ playwright.config.ts
@@ -11,7 +11,7 @@ import { defineConfig, devices } from "@playwright/test";
  */
 export default defineConfig({
   testDir: "./tests",
-  timeout: 10 * 1000,
+  timeout: process.env.CI ? 30 : 10 * 1000,
   expect: {
     /**
      * Maximum time expect() should wait for the condition to be met.
