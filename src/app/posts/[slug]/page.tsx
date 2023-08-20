dedd197221fd4361567aff18cf6f56ab8bff8427
Nicholas Romero <ncrmro@gmail.com>
2023-08-20 16:30:14 -0600

    fix:

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -11,26 +11,39 @@ export default async function PostPage({
   params: { slug: string };
 }) {
   const viewer = await useViewer();
-  const post = await db
-    .selectFrom("posts")
-    .select([
-      "id",
-      "title",
-      "description",
-      "body",
-      "slug",
-      "publish_date",
-      "published",
-    ])
-    .where("slug", "=", params.slug)
-    .executeTakeFirst();
+  // Kysely has a better function to aggregate sub query but sqlite version wasn't exported?
+  // https://github.com/kysely-org/kysely/issues/628
+  const [post, tags] = await Promise.all([
+    db
+      .selectFrom("posts")
+      .select([
+        "id",
+        "title",
+        "description",
+        "body",
+        "slug",
+        "published",
+        "publish_date",
+      ])
+      .where("slug", "=", params.slug)
+      .executeTakeFirstOrThrow(),
+    db
+      .selectFrom("tags")
+      .select(["tags.id", "tags.value"])
+      .innerJoin("posts_tags", "posts_tags.tag_id", "tags.id")
+      .innerJoin("posts", "posts.id", "posts_tags.post_id")
+      .where("posts.slug", "=", params.slug)
+      .distinct()
+      .execute(),
+  ]);
   // If viewer is not logged and and post is not published also not found
   if (!post || (post.published === 0 && !viewer)) notFound();
-  const mdxSource = await serializePost(post);
+  const p = { ...post, tags };
+  const mdxSource = await serializePost(p);
 
   return (
     <div className="w-full md:max-w-4xl">
-      <Post viewer={viewer} post={post} source={mdxSource} />
+      <Post viewer={viewer} post={p} source={mdxSource} />
     </div>
   );
 }
