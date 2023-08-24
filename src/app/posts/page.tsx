f69423ba348c38f7ecd908207f77ea05b8fdfd60
Nicholas Romero <ncrmro@gmail.com>
2023-08-24 15:20:24 -0600

    feat: show drafts using yellow pill

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -16,13 +16,19 @@ export function PostItem(props: {
   return (
     <li key={props.post.slug} className="p-2">
       <div>
-        <a
-          href={`/posts/${props.post.slug}`}
-          className="font-medium text-gray-900"
-        >
-          {!props.post.published && "Draft!"}
-          {props.post.title}
-        </a>
+        <div className="flex justify-between">
+          <a
+            href={`/posts/${props.post.slug}`}
+            className="font-medium text-gray-900"
+          >
+            {props.post.title}
+          </a>
+          {!props.post.published && (
+            <span className="inline-flex items-center rounded-full bg-yellow-50 px-2 py-1 text-xs font-medium text-yellow-800 ring-1 ring-inset ring-yellow-600/20">
+              Draft
+            </span>
+          )}
+        </div>
         <p className="mt-0.5 text-sm text-gray-500">
           {props.post.publish_date && formatDate(props.post.publish_date)}
         </p>
