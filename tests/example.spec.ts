4fb7485e5998cfbebeb4c9d2cae4a6a435758569
Nicholas Romero <ncrmro@gmail.com>
2023-08-24 11:16:50 -0600

    chore: delete signup page

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -28,17 +28,6 @@ test("login page", async ({ page }) => {
   await page.waitForURL("/dashboard");
 });
 
-test("sign up page", async ({ page }) => {
-  const time = Date.now();
-  const username = `user${time}`;
-  const email = `${username}@test.com`;
-  await page.goto("/signup");
-  await page.getByPlaceholder("email").fill(email);
-  await page.getByPlaceholder("password").fill("testpassword");
-  await page.locator("button", { hasText: "Submit" }).click();
-  await page.waitForURL("/dashboard");
-});
-
 test("create posts", async ({ page }) => {
   await page.goto("/posts/new");
   await page.waitForURL((url) =>
