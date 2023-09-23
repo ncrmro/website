8666eacfd4c858cfdcf766fa0bc48788afaf495d
Nicholas Romero <ncrmro@gmail.com>
2023-09-23 16:52:15 -0500

    refactor: move post form media into own file

---
+++ src/app/dashboard/posts/form_media.tsx
@@ -0,0 +1,28 @@
+import { default as NextImage } from "next/image";
+import React, { useState } from "react";
+import { PostType } from "../../posts/types";
+
+export default function PostMedia(props: { post: PostType }) {
+  const [files, setFiles] = useState([]);
+  React.useEffect(() => {
+    fetch(`/api/posts/uploads?postId=${props.post.id}`).then(async (res) => {
+      const data = await res.json();
+      setFiles(data.files as []);
+    });
+  }, []);
+  return (
+    <div>
+      {files.map((file) => (
+        <div key={file}>
+          {file}
+          <NextImage
+            alt=""
+            width={500}
+            height={500}
+            src={`/uploads/posts/${props.post.id}/${file}`}
+          />
+        </div>
+      ))}
+    </div>
+  );
+}
