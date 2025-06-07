5935a1733692837fbd23f1f87ccc68ab223dcabc
Nicholas Romero <ncrmro@gmail.com>
2025-06-07 10:18:52 -0500

    feat: add umami analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import GoogleAnalytics from "@/components/GoogleAnalytics";
+import UmamiAnalytics from "@/components/UmamiAnalytics";
 import React from "react";
 
 export const metadata = {
@@ -15,6 +16,11 @@ export default async function RootLayout(props: { children: React.ReactNode }) {
     <html lang="en" className="dark:bg-gray-900 dark:text-white">
       <body>
         {isProduction && <GoogleAnalytics />}
+        <UmamiAnalytics
+          scriptUrl="https://umami.ncrmro.com/script.js"
+          websiteId="8b476bdf-8835-4f55-8afc-9a4d78d16b13"
+          domains={["ncrmro.com", "www.ncrmro.com"]}
+        />
         {props.children}
       </body>
     </html>
